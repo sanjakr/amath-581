0760b7745ea2b3a7eb88bda480b8a7ebce4daa7a
sanjakr2 <sanjakr2@cisco.com>
2024-10-18 13:22:45 -0500

    Testing changes

--- 1.py
+++ 1.py
@@ -8,12 +8,12 @@ def shoot2(y, x, beta):
     return [y[1], (x**2 - beta) * y[0]]
 
 
-tol = 1e-6  # define a tolerance level
+tol = 1e-4  # define a tolerance level
 col = ['r', 'b', 'g', 'c', 'm', 'k']  # eigenfunc colors
 n0 = 1
 A = -1
-x0 = [0, A]
 L = 4
+# x0 = [1, np.sqrt(L*L - n0)]
 xp = [-L, L]
 xshoot = np.linspace(xp[0], xp[1], 81)
 ys = []
@@ -22,32 +22,38 @@ evs = []
 beta_start = n0  # beginning value of beta
 for modes in range(1, 6):  # begin mode loop
     beta = beta_start  # initial value of eigenvalue beta
-    dbeta = n0 / 10  # default step size in beta
+    x0 = [1, np.sqrt(L**2 - beta)]
+    dbeta = beta / 100  # default step size in beta
     for i in range(1000):  # begin convergence loop for beta
         y_sol = odeint(shoot2, x0, xshoot, args=(beta,))
         # y = RK45(shoot2, xp[0], x0, xp[1], args=(n0,beta))
+        cond = y_sol[-1, 1] + np.sqrt(L**2 - beta)*y_sol[-1, 0]
 
-        if abs(y_sol[-1, 0] - 0) < tol:  # check for convergence
+        if i == 1 or i == 999:
+            print(cond)
+
+        if abs(cond) < tol:  # check for convergence
             print(i)
             print(beta)  # write out eigenvalue
             evs.append(beta)
             break  # get out of convergence loop
 
-        if (-1) ** (modes + 1) * y_sol[-1, 0] > 0:
+        if (-1) ** (modes + 1) * cond > 0:
             beta -= dbeta
         else:
-            beta += dbeta / 2
-            dbeta /= 2
+            beta += dbeta / 10
+            dbeta /= 10
 
+    print(beta)
     beta_start = beta + 2  # after finding eigenvalue, pick new start
     norm = np.trapezoid(y_sol[:, 0] * y_sol[:, 0], xshoot)  # calculate the normalization
-    y_norm = np.abs(y_sol[:, 0] / np.sqrt(norm))
+    y_norm = np.abs(y_sol[:, 0]) / np.sqrt(norm)
     ys.append(y_norm)
-#     plt.plot(xshoot, y_norm, col[modes - 1])  # plot modes
-#
-# plt.show()  # end mode loop
+    plt.plot(xshoot, y_norm, col[modes - 1])  # plot modes
+
+plt.show()  # end mode loop
 
 A1 = np.transpose(np.array(ys))
 A2 = np.array(evs)
-print(A1.shape)
-print(A2.shape)
+# print(A1)
+print(A2)
